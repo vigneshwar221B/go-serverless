2f05f53b51a7234cd79573956edea216acc648d4
Vignesh War <vigneshwar@Vignes.local>
2021-12-07 11:28:50 +0530

    added a generic login page

---
+++ src/main/java/com/presidio/project/controller/HotelController.java
@@ -0,0 +1,9 @@
+package com.presidio.project.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HotelController {
+    @GetMapping(value="/{hotel_name}/login")
+    String getCustomerLogin(){ return "hotel/login";}
+}
